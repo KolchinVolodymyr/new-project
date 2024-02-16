6a19211168a5d46035a84e5ba2a1bda5755320e9
Volodymyr Kolchin <KolchinVolo@gmail.com>
2024-02-17 01:05:14 +0200

    change wp-activate.php

--- wp-activate.php
+++ wp-activate.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Confirms that the activation key that is sent in an email after a user signs
- * up for a new site matches the key for that user and then displays confirmation.
+ * Confirms that the activation key.
  *
  * @package WordPress
  */
